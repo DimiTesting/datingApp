9f0d6e132ca2aacff37f8bc24172fe3f2649d9ae
amankeldi.kurban <dimitesting31@gmail.com>
2025-07-22 20:18:17 +0300

    Setting the skeleton of client side with Angular, Tailwind and DaisyUi

---
+++ Client/src/app/app.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, Signal, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App implements OnInit {
+  private http : HttpClient = inject(HttpClient);
+  protected readonly title = 'Dating App';
+  protected members = signal<any>([])
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/members').subscribe({
+      next: response => this.members.set(response),
+      error: error => console.log(error),
+      complete: () => console.log('Completed http request')
+    })
+  }
+}
